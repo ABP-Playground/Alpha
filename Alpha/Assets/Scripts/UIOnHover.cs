5bda7abbea56251a3611377205c03a21051b265a
nahtanoJP <Jon_p3rron@hotmail.com>
2017-04-16 19:17:05 -0400

    Main Menu Created
    - Created main menu
    - Added 3 UI Text objects to main menu
    - OnClick play takes user to scene Main
    - Added fade inbetween scenes
    - Added game manager, and made it singleton

---
+++ Alpha/Assets/Scripts/UIOnHover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIOnHover : MonoBehaviour {
+
+	public void OnHover() {
+		gameObject.GetComponent<Text> ().color = Color.green;
+	}
+
+	public void OnExit() {
+		gameObject.GetComponent<Text> ().color = Color.white;
+	}
+
+	public void OnClickPlay() {
+		StartCoroutine (SceneChange());
+	}
+
+	IEnumerator SceneChange() {
+		float fadeTime = GameObject.Find ("GameManager").GetComponent<Fade>().BeginFade(1);
+		yield return new WaitForSeconds(fadeTime);
+		SceneManager.LoadScene (1);
+	}
+}
